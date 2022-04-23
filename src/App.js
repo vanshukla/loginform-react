87bc3b50394b904331c19984efd8115dc4dcdb94
Vanpriya Shukla <shukla.vanpriya@gmail.com>
2022-04-24 01:15:04 +0530

    deploy to netlify

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import LoginForm from './components/LoginForm';
 
 function App() {
+
+  const adminUser = {
+    email: "shukla.vanpriya@gmail.com",
+    password: "admin123"
+  };
+
+  const [user, setUser] = useState({ name: "", email: "" });
+  const [error, setError] = useState("");
+
+  const login = details => {
+    console.log(details);
+    if (details.email == adminUser.email && details.password == adminUser.password) {
+      console.log("You are logged in!");
+      setUser({
+        name: details.name,
+        email: details.email
+      });
+    } else {
+      console.log("Details do not match. Try again!");
+      setError("Details do not match. Try Again!");
+    }
+  }
+
+  const logout = () => {
+    console.log("Logout");
+    setUser({
+      name: "",
+      email: ""
+    });
+    setError("");
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {(user.email != "") ? (
+        <div className="welcome">
+          <h2>Welcome, <span>{user.name}</span></h2>
+          <button onClick={logout}>Logout</button>
+        </div>
+      ) : (
+          <LoginForm Login={login} error={error} />
+        )}
+
     </div>
   );
 }
